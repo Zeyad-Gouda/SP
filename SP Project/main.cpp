6755f069e2010f3d43bf07f454735b98fde6275e
ZeyadGouda <zm41204@gmail.com>
2026-03-03 03:52:12 +0200

    test

---
+++ SP Project/main.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+int main()
+{
+    RenderWindow window(VideoMode({800, 600}), "Title");
+    CircleShape circle(50.f);
+    circle.setFillColor(Color::White);
+    circle.setOrigin(Vector2f(50.f, 50.f));
+    circle.setPosition(Vector2f(400.f, 300.f));
+
+    float speed = 400.f; // pixels per second
+    Clock clock;
+
+    while (window.isOpen())
+    {
+        float deltaTime = clock.restart().asSeconds();
+
+        while (auto event = window.pollEvent())
+            if (event->is<Event::Closed>())
+                window.close();
+
+        if (Keyboard::isKeyPressed(Keyboard::Key::W))
+            circle.move(Vector2f(0.f, -speed * deltaTime));
+        if (Keyboard::isKeyPressed(Keyboard::Key::S))
+            circle.move(Vector2f(0.f, speed * deltaTime));
+        if (Keyboard::isKeyPressed(Keyboard::Key::A))
+            circle.move(Vector2f(-speed * deltaTime, 0.f));
+        if (Keyboard::isKeyPressed(Keyboard::Key::D))
+            circle.move(Vector2f(speed * deltaTime, 0.f));
+
+        window.clear(Color(0, 0, 0));
+        window.draw(circle);
+        window.display();
+    }
+    return 0;
+}
